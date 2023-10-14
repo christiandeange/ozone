6bd4593d2dbc5d744175ecbb3ff3dc0f35871df8
Christian De Angelis <christiandeange@gmail.com>
2023-10-14 00:20:42 -0400

    Support legacy-style Blobs as well.

--- api-gen-runtime/src/commonMain/kotlin/sh/christian/ozone/api/model/Blob.kt
+++ api-gen-runtime/src/commonMain/kotlin/sh/christian/ozone/api/model/Blob.kt
@@ -1,21 +1,45 @@
 package sh.christian.ozone.api.model
 
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.cbor.ByteString
+import kotlinx.serialization.json.JsonContentPolymorphicSerializer
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonObject
 
-@OptIn(ExperimentalSerializationApi::class)
 @SerialName("blob")
-@Serializable
-data class Blob(
-  @ByteString val ref: BlobRef,
-  val mimeType: String,
-  val size: Long,
-)
+@Serializable(with = BlobSerializer::class)
+sealed interface Blob {
+
+  @OptIn(ExperimentalSerializationApi::class)
+  @Serializable
+  data class StandardBlob(
+    @ByteString val ref: BlobRef,
+    val mimeType: String,
+    val size: Long,
+  ) : Blob
+
+  @Serializable
+  data class LegacyBlob(
+    val cid: String,
+    val mimeType: String,
+  ) : Blob
+}
 
 @Serializable
 data class BlobRef(
   @SerialName("\$link")
   val link: String,
 )
+
+class BlobSerializer : JsonContentPolymorphicSerializer<Blob>(Blob::class) {
+  override fun selectDeserializer(element: JsonElement): DeserializationStrategy<Blob> {
+    return if (element.jsonObject.containsKey("ref")) {
+      Blob.StandardBlob.serializer()
+    } else {
+      Blob.LegacyBlob.serializer()
+    }
+  }
+}
