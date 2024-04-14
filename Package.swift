c6a648c1652f0474f608f50d1a996bd4f38ab409
Christian De Angelis <christiandeange@gmail.com>
2024-04-14 00:34:31 -0400

    Prepare next development cycle.

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://repo1.maven.org/maven2/sh/christian/ozone/bluesky-kmmbridge/0.0.9/bluesky-kmmbridge-0.0.9.zip"
-let remoteKotlinChecksum = "caea969892d98fd9165ca2afecbd8d7d5d5888223c0c37f74e44d2cf1afbdf6b"
 let packageName = "BlueskyAPI"
 
 let package = Package(
@@ -19,8 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            url: remoteKotlinUrl,
-            checksum: remoteKotlinChecksum
+            path: "./bluesky/build/XCFrameworks/debug/\(packageName).xcframework"
         )
         ,
     ]
