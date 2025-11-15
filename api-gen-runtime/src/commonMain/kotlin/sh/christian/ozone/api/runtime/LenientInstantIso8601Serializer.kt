f620556ff7588824a0a64530b94465d8360801ef
cbruegg <mail@cbruegg.com>
2025-11-15 14:45:53 +0100

    Update kotlinx-datetime to 0.7.1

--- api-gen-runtime/src/commonMain/kotlin/sh/christian/ozone/api/runtime/LenientInstantIso8601Serializer.kt
+++ api-gen-runtime/src/commonMain/kotlin/sh/christian/ozone/api/runtime/LenientInstantIso8601Serializer.kt
@@ -1,12 +1,12 @@
 package sh.christian.ozone.api.runtime
 
-import kotlinx.datetime.Instant
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
+import kotlin.time.Instant
 
 object LenientInstantIso8601Serializer : KSerializer<Instant> {
   override val descriptor: SerialDescriptor =
